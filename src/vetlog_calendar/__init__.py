4b6ef01153a781d41b83c9622ba95756f837bba2
josdem <joseluis.delacruz@gmail.com>
2026-03-05 07:35:44 -0600

    Setting version

--- src/vetlog_calendar/__init__.py
+++ src/vetlog_calendar/__init__.py
@@ -1,10 +1,3 @@
-from importlib.metadata import version
-
-"""
-Python convention:
-- project name is the human-readable name using dashes: "vetlog-buddy"
-- package name is the importable name using underscores: "vetlog_buddy"
-"""
 # Get project info from pyproject.toml
 __project__ = "vetlog-calendar"
-__version__ = version(__project__)
+__version__ = "1.0.0"
