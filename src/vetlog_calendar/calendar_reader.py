7a75c389981f4e3f974a9f62c85ef6a4028632f2
josdem <joseluis.delacruz@gmail.com>
2026-03-09 12:52:11 -0600

    Saving token path

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -4,7 +4,8 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-CREDENTIALS_PATH = "/home/josdem/.calendar/token.json"
+TOKEN_PATH = "/home/josdem/.calendar/token.json"
+CREDENTIALS_PATH = "/home/josdem/.calendar/credentials.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 
@@ -12,17 +13,17 @@ class Reader:
     def listing_events(self):
         print("Listing events")
         creds = None
-        if os.path.exists(CREDENTIALS_PATH):
-            creds = Credentials.from_authorized_user_file(CREDENTIALS_PATH, SCOPES)
+        if os.path.exists(TOKEN_PATH):
+            creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
         if not creds or not creds.valid:
             print("No valid credentials")
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    "/home/josdem/.calendar/credentials.json", SCOPES
+                    CREDENTIALS_PATH, SCOPES
                 )
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open("token.json", "w") as token:
+            with open(TOKEN_PATH, "w") as token:
                 token.write(creds.to_json())
