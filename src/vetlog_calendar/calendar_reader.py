5a08a76877545fbe25b3e12cb51feae37d36456f
josdem <joseluis.delacruz@gmail.com>
2026-03-09 12:39:12 -0600

    Creating Google credentials if does not exits

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -1,8 +1,10 @@
 import os.path
 
 from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-CREDENTIALS_PATH = "/home/josdem/.calendar/credentials.json"
+CREDENTIALS_PATH = "/home/josdem/.calendar/token.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 
@@ -14,3 +16,13 @@ class Reader:
             creds = Credentials.from_authorized_user_file(CREDENTIALS_PATH, SCOPES)
         if not creds or not creds.valid:
             print("No valid credentials")
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "/home/josdem/.calendar/credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
