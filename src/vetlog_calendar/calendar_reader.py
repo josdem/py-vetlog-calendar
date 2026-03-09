f0db87ec113e2b0472c2d8916dfa64dfc6955527
josdem <joseluis.delacruz@gmail.com>
2026-03-09 13:39:32 -0600

    Printing upcoming events

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -1,8 +1,11 @@
 import os.path
+import datetime
 
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 TOKEN_PATH = "/home/josdem/.calendar/token.json"
 CREDENTIALS_PATH = "/home/josdem/.calendar/credentials.json"
@@ -27,3 +30,33 @@ class Reader:
             # Save the credentials for the next run
             with open(TOKEN_PATH, "w") as token:
                 token.write(creds.to_json())
+        try:
+            service = build("calendar", "v3", credentials=creds)
+
+            # Call the Calendar API
+            now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+            print("Getting the upcoming 10 events")
+            events_result = (
+                service.events()
+                .list(
+                    calendarId="primary",
+                    timeMin=now,
+                    maxResults=10,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+            events = events_result.get("items", [])
+
+            if not events:
+                print("No upcoming events found.")
+                return
+
+            # Prints the start and name of the next 10 events
+            for event in events:
+                start = event["start"].get("dateTime", event["start"].get("date"))
+                print(start, event["summary"])
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
