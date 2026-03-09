4fc65e9f6e83496096e61b001292a8b021ac1b4f
josdem <joseluis.delacruz@gmail.com>
2026-03-09 11:40:38 -0600

    Trying to read credentials

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -1,11 +1,14 @@
 import os.path
 
+from google.oauth2.credentials import Credentials
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 class Reader:
     def listing_events(self):
         print("Listing events")
         creds = None
         if os.path.exists("token.json"):
-            print("Token file exists")
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
         if not creds or not creds.valid:
             print("No valid credentials")
