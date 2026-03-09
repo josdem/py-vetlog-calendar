c5508feb0aba02f359620cd7fe95247da6f4e90a
josdem <joseluis.delacruz@gmail.com>
2026-03-09 11:29:18 -0600

    Importing os to read credentials

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -1,3 +1,11 @@
+import os.path
+
+
 class Reader:
     def listing_events(self):
         print("Listing events")
+        creds = None
+        if os.path.exists("token.json"):
+            print("Token file exists")
+        if not creds or not creds.valid:
+            print("No valid credentials")
