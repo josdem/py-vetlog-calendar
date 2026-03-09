b0bcf8db5fc78e6b9faf145b01b53226459fde06
josdem <joseluis.delacruz@gmail.com>
2026-03-09 12:19:50 -0600

    Hardcoding credentials path

--- src/vetlog_calendar/calendar_reader.py
+++ src/vetlog_calendar/calendar_reader.py
@@ -2,13 +2,15 @@ import os.path
 
 from google.oauth2.credentials import Credentials
 
+CREDENTIALS_PATH = "/home/josdem/.calendar/credentials.json"
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
 
 class Reader:
     def listing_events(self):
         print("Listing events")
         creds = None
-        if os.path.exists("token.json"):
-            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        if os.path.exists(CREDENTIALS_PATH):
+            creds = Credentials.from_authorized_user_file(CREDENTIALS_PATH, SCOPES)
         if not creds or not creds.valid:
             print("No valid credentials")
