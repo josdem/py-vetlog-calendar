9c5c1a8826bf4cd688f1074efb4057b4a335e065
josdem <joseluis.delacruz@gmail.com>
2026-06-15 14:50:24 -0600

    Searching for pet logs in two days

--- src/vetlog_calendar/main.py
+++ src/vetlog_calendar/main.py
@@ -208,7 +208,7 @@ def list_surgeries_without_logs(
         calendar = Calendar()
 
     end_date = datetime.now()
-    start_date = end_date - timedelta(days=3)
+    start_date = end_date - timedelta(days=2)
 
     surgeries = calendar.list_surgeries()
 
