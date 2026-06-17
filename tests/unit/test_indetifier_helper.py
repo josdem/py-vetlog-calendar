d10b639efe96b38309fd1f5efb8dba46ef689cf8
josdem <joseluis.delacruz@gmail.com>
2026-06-17 10:47:57 -0600

    #125 Adding paarothecoder suggestion

--- tests/unit/test_indetifier_helper.py
+++ tests/unit/test_indetifier_helper.py
@@ -12,12 +12,14 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import pytest
+
 from vetlog_calendar.shared.identifier_helper import get_vetlog_id
 
 
-def test_get_vetlog_id():
-
-    event = {
+@pytest.fixture
+def event():
+    return {
         "summary": "Jose - Vaccination appointment for Sora",
         "location": "Whatever works for you",
         "description": """Jose Morales\n1234567890\n\nVetlogID: 338\nSurgery appointment for Sora\nVaccine type: C6CV\n\nThank you for trusting Vetlog!\nhttps://vetlog.org/""",
@@ -37,30 +39,16 @@ def test_get_vetlog_id():
         ],
     }
 
+
+def test_get_vetlog_id(event):
     assert get_vetlog_id(event["description"]) == "338"
 
 
-def test_get_vetlog_id_not_found():
+def test_get_vetlog_id_not_found(event):
 
-    event = {
-        "summary": "Jose - Vaccination appointment for Sora",
-        "location": "Whatever works for you",
-        "description": """Jose Morales\n1234567890\n\nSurgery appointment for Sora\nVaccine type: C6CV\n\nThank you for trusting Vetlog!\nhttps://vetlog.org/""",
-        "start": {
-            "dateTime": "2026-05-21T11:00:00-06:00",
-            "timeZone": "UTC",
-        },
-        "end": {
-            "dateTime": "2026-05-21T11:15:00-06:00",
-            "timeZone": "UTC",
-        },
-        "attendees": [
-            {"email": "contact@josdem.io"},
-            {"email": "email1@example.com"},
-            {"email": "email2@example.com"},
-            {"email": "email3@example.com"},
-        ],
-    }
+    event["description"] = (
+        """Jose Morales\n1234567890\n\nSurgery appointment for Sora\nVaccine type: C6CV\n\nThank you for trusting Vetlog!\nhttps://vetlog.org/"""
+    )
 
     try:
         get_vetlog_id(event["description"])
@@ -69,26 +57,8 @@ def test_get_vetlog_id_not_found():
         pass
 
 
-def test_get_vetlog_id_if_lower_case():
-
-    event = {
-        "summary": "Jose - Vaccination appointment for Sora",
-        "location": "Whatever works for you",
-        "description": """Jose Morales\n1234567890\n\nvetlogId: 338\nSurgery appointment for Sora\nVaccine type: C6CV\n\nThank you for trusting Vetlog!\nhttps://vetlog.org/""",
-        "start": {
-            "dateTime": "2026-05-21T11:00:00-06:00",
-            "timeZone": "UTC",
-        },
-        "end": {
-            "dateTime": "2026-05-21T11:15:00-06:00",
-            "timeZone": "UTC",
-        },
-        "attendees": [
-            {"email": "contact@josdem.io"},
-            {"email": "email1@example.com"},
-            {"email": "email2@example.com"},
-            {"email": "email3@example.com"},
-        ],
-    }
-
+def test_get_vetlog_id_if_lower_case(event):
+    event["description"] = (
+        """Jose Morales\n1234567890\n\nvetlogId: 338\nSurgery appointment for Sora\nVaccine type: C6CV\n\nThank you for trusting Vetlog!\nhttps://vetlog.org/"""
+    )
     assert get_vetlog_id(event["description"]) == "338"
